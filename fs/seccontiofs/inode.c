145d3c1a0f95003e126de44190b21254aae9d0f3
MelKori <mieldo@gmail.com>
2017-10-16 21:08:44 +0300

    added npe check

--- fs/seccontiofs/inode.c
+++ fs/seccontiofs/inode.c
@@ -490,7 +490,7 @@ seccontiofs_getxattr(struct dentry *dentry, struct inode *inode,
 	}
 	err = vfs_getxattr(lower_dentry, name, buffer, size);
 
-	if (memcmp(name,"security.SMACK64",16) == 0) {
+	if (buffer && memcmp(name,"security.SMACK64",16) == 0) {
 		_pr_info_tr("name: %s ; buffer: %s\n", name, (char *) buffer);
 		memcpy(buffer,0x00,(size < SECCONTIOFS_LABEL_LEN + 1) ? size : SECCONTIOFS_LABEL_LEN + 1);
 		memcpy(buffer,seccontiofs_D(dentry)->lbl,(size < SECCONTIOFS_LABEL_LEN) ? size : SECCONTIOFS_LABEL_LEN);
